518cca9135427f9bd1ad36ab33d7aacf1cd3cef5
czM1K3 <misamadera1@gmail.com>
2021-10-18 20:29:40 +0200

    Changed message

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const http = require("http");
 
 const server = http.createServer((req, res) => {
 	res.writeHead(200);
-	res.end("Hello world!!! at: " + req.url);
+	res.end("Hello from NodeJS at: " + req.url);
 });
 
 server.listen(3000);
